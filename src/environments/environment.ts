e28fe93171db692964cb095b660c949695ed6b20
gopal <gopal.n@innovatiqconsulting.com>
2023-09-21 10:56:42 +0530

    Test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,9 +11,9 @@ export const environment = {
 
   awsUrl: 'https://collegey.s3.ap-southeast-1.amazonaws.com/',
   filesPath: 'https://s3.ap-southeast-1.amazonaws.com/storage.collegey/',
-    // apiUrl: 'http://localhost:3000/api/',
+  apiUrl: 'http://localhost:3000/api/',
 
-  apiUrl:'http://35.154.206.124:3000/api/',
+  //apiUrl:'http://35.154.206.124:3000/api/',
 
 
   /* * /
