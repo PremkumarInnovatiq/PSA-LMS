960712c43477ee705fad32dd6f03e7e27b986b6a
Vaishnavi Kaithepalli <vaishnavi@innovatiqconsulting.com>
2023-10-13 13:09:27 +0530

    email-configuration-integration

--- src/app/admin/email-configuration/instructor-accept-course-invite/instructor-accept-course-invite.component.ts
+++ src/app/admin/email-configuration/instructor-accept-course-invite/instructor-accept-course-invite.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { EmailConfigService } from '@core/service/email-config.service';
+import { UtilsService } from '@core/service/utils.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-instructor-accept-course-invite',
@@ -6,5 +12,123 @@ import { Component } from '@angular/core';
   styleUrls: ['./instructor-accept-course-invite.component.scss']
 })
 export class InstructorAcceptCourseInviteComponent {
+ 
+  edit = true;
+  emailTemplateForm!: FormGroup;
+  id: any;
+  itemData: any; 
+  pageContent: any;
+  isLoading = false;
+  assignData = [];
+  isSubmitted=false;
 
+  public Editor: any = ClassicEditor;
+
+  breadscrums = [
+    {
+      title: 'Forgot Mail',
+      items: ['Email Configuration'],
+      active: 'Instructor Accept Course Invite Status ',
+    },
+  ];
+  constructor(
+    private fb: FormBuilder,
+    public utils:UtilsService, 
+    private emailConfigurationService: EmailConfigService, 
+    private ref: ChangeDetectorRef,
+    private activatedRoute: ActivatedRoute,
+   
+  ) { }
+
+  ngOnInit(): void {
+    this.initialize();
+    
+      this.getForgetPasswordTemplate();
+   
+  }
+
+  toggle(_data: any){
+    this.edit =!this.edit;
+    // this._id = _data._id;
+    this.emailTemplateForm.patchValue({
+      email_subject: _data.email_subject,
+      email_top_header_text:_data.email_top_header_text,
+      email_content:_data.email_content,
+     
+    });
+
+  }
+  initialize() {
+    this.createForm();
+    
+  }
+  back(){
+    this.edit =!this.edit;
+  }
+  fetchUpdated() {
+    this.patchForm(this.itemData);
+  }
+
+  patchForm(pageContent: any) {
+    this.pageContent = pageContent;
+    this.emailTemplateForm.patchValue({
+      email_subject: pageContent?.email_subject,
+      email_top_header_text: pageContent?.email_top_header_text,
+      email_content: pageContent?.email_content,
+    });
+    // this.markAllTouched();
+  }
+
+  createForm() {
+    this.emailTemplateForm = this.fb.group(
+      {
+        email_subject: ['', [Validators.required]],
+        email_top_header_text: ['', [Validators.required]],
+        email_content: ['', [Validators.required]],
+        
+      },
+    );
+    this.emailTemplateForm.valueChanges.subscribe(() => {
+    });
+  }
+
+  getForgetPasswordTemplate() {
+    this.emailConfigurationService.getForgetPasswordTemplate().subscribe( response =>{
+      this.assignData  = response?.data?.docs[0]?.mentor_accept_project_invite_status_template;
+      this.ref.detectChanges();
+  }
+)}
+  
+  update(){
+    return new Promise<void>((resolve, reject) => {
+      // this.markAllTouched();
+      if (this.emailTemplateForm.valid) {
+
+          let obj = this.emailTemplateForm.value;
+          obj['insertaction'] = 'mentor_accept_project_invite_status_template';
+          this.emailConfigurationService.updateForgetPasswordTemplate(obj,this.id).subscribe(
+            (res) => {
+              Swal.fire({
+                title: 'Successful',
+                text: 'Update data Succesfully',
+                icon: 'success',
+              });
+              this.back();
+              resolve();
+              this.getForgetPasswordTemplate();
+            },
+            (err) => {
+              Swal.fire(
+                'Failed to Update',
+                'error'
+              );
+              reject();
+            },
+            () => {
+              reject();
+            }
+          );
+        }
+      }
+    )};
 }
--- src/app/admin/email-configuration/send-course-invoice/send-course-invoice.component.ts
+++ src/app/admin/email-configuration/send-course-invoice/send-course-invoice.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { EmailConfigService } from '@core/service/email-config.service';
+import { UtilsService } from '@core/service/utils.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-send-course-invoice',
@@ -6,5 +12,127 @@ import { Component } from '@angular/core';
   styleUrls: ['./send-course-invoice.component.scss']
 })
 export class SendCourseInvoiceComponent {
+  edit = true;
+  emailTemplateForm!: FormGroup;
+  assignData :any[] = [];
+  isSubmitted=false;
+  id: any;
+  itemData: any;
+  pageContent: any;
+  isLoading = false;
+  show_loader: boolean = false;
+  public Editor: any = ClassicEditor;
 
+  breadscrums = [
+    {
+      title: 'Forgot Mail',
+      items: ['Email Configuration'],
+      active: 'Send Course Invoice',
+    },
+  ];
+  constructor(
+    private router: Router,  
+    public utils:UtilsService,  
+    private emailConfigurationService: EmailConfigService,
+     private fb: FormBuilder,
+     private activatedRoute: ActivatedRoute,
+     private ref: ChangeDetectorRef
+    ) {
+    
+   
+  }
+  
+  toggle(_data: any){
+    this.edit =!this.edit;
+    // this._id = _data._id;
+    this.emailTemplateForm.patchValue({
+      email_subject: _data.email_subject,
+      email_top_header_text:_data.email_top_header_text,
+      email_content:_data.email_content,
+    });
+
+  }
+  
+  back(){
+    this.edit =!this.edit;
+  }
+  ngOnInit(): void {
+    this.initialize();
+   
+      this.getForgetPasswordTemplate();
+   
+  }
+
+  initialize() {
+    this.createForm();
+    
+  }
+
+  fetchUpdated() {
+    this.patchForm(this.itemData);
+  }
+
+  patchForm(pageContent: any) {
+    this.pageContent = pageContent;
+    this.emailTemplateForm.patchValue({
+      // email_template_type: pageContent?.email_template_type,
+      email_subject: pageContent?.email_subject,
+      email_top_header_text: pageContent?.email_top_header_text,
+      email_content: pageContent?.email_content,
+      // bottom_button_text: pageContent?.bottom_button_text,
+    });
+  
+  }
+
+  createForm() {
+    this.emailTemplateForm = this.fb.group(
+      {
+        email_subject: ['', [Validators.required]],
+        email_top_header_text: ['', [Validators.required]],
+        email_content: ['', [Validators.required]],
+        bottom_button_text: ['', [Validators.required]],
+      },
+    );
+    this.emailTemplateForm.valueChanges.subscribe(() => {
+    });
+  }
+  getForgetPasswordTemplate() {
+    this.emailConfigurationService.getForgetPasswordTemplate().subscribe( response =>{
+      this.assignData  = response?.data?.docs[0]?.send_project_invoice_template;
+      this.ref.detectChanges();
+    
+  }
+  )}
+ 
+  
+  update(){
+  return new Promise<void>((resolve, reject) => {
+        const obj = this.emailTemplateForm.value;
+        obj.insertaction = 'send_project_invoice_template';
+        this.emailConfigurationService.updateForgetPasswordTemplate(obj, this.id).subscribe(
+          (res) => {
+            console.log(res);
+            Swal.fire({
+              title: 'Successful',
+              text: 'Update data Succesfully',
+              icon: 'success',
+            });
+            this.back();
+            resolve();
+            this.getForgetPasswordTemplate();
+          },
+          (err) => {
+            Swal.fire(
+              'Failed to Update',
+              'error'
+            );
+            reject();
+          },
+          () => {
+            reject();
+          }
+        );
+      }
+  
+   )};
 }
